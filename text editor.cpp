340551e9ae99c4f2840191af053e64a3963e4f26
MaxKreitzer <201699@pennmanor.net>
2021-04-09 09:05:10 -0400

    Text editor

--- text editor.cpp
+++ text editor.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <iostream>
+#include <fstream>
 using namespace std;
 int main(){
-  
+  string type;
+  cin >> type;
+  ofstream textfile;
+  textfile.open("typefile.odt");
+  textfile << type;
+  textfile.close();
+  return 0;
 }
